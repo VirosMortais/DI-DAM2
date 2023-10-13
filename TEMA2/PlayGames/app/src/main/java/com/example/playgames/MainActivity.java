d28d87f4fc03fdf2b8fdd26128c394233597bcc6
chaarrsan <charlestacodeouro@hotmail.com>
2023-10-13 20:02:20 +0200

    MainActivity modificado para llamar la clase NewPlayer

--- TEMA2/PlayGames/app/src/main/java/com/example/playgames/MainActivity.java
+++ TEMA2/PlayGames/app/src/main/java/com/example/playgames/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.playgames;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,7 +17,10 @@ public class MainActivity extends AppCompatActivity {
         Button newPlayerButton = findViewById(R.id.newPlayerButton);
 
         newPlayerButton.setOnClickListener(v -> {
-            setContentView(R.layout.activity_new_player);
+            Intent i = new Intent(this, NewPlayer.class);
+            startActivity(i);
         });
     }
+
+
 }
